406173ab1e97154b1368128da38b2e89ca8e1059
Chris M <821688+tebriel@users.noreply.github.com>
2022-01-21 21:00:56 +0000

    Turn off kusto for now

--- kusto.tf
+++ kusto.tf
@@ -45,6 +45,7 @@ data "azurerm_subscription" "primary" {}
 data "azurerm_client_config" "test" {}
 
 resource "azurerm_role_assignment" "test-eventhub" {
+  count                = var.kusto_enable
   scope                = data.azurerm_subscription.primary.id
   role_definition_name = "Reader"
   principal_id         = data.azurerm_client_config.test.object_id
